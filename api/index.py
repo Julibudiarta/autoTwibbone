f5a70535c44c773f508d103924df83f2951bbd1a
Julibudiarta <julibudiarta25@gmail.com>
2026-08-22 20:57:35 +0800

    fix: vercel static routes and cwd initialization

--- api/index.py
+++ api/index.py
@@ -5,6 +5,7 @@ import sys
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
+os.chdir(root_dir)
 
 from app import app
 
